279fdeeca70362652ab32e110743bc953578d4f8
moohamedmagdi <moohamedmagdi@gmail.com>
2023-03-16 12:33:13 -0700

    hi there

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: print with printf
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
