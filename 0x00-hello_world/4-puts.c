d55b98c14c7d8978a6c96ec8bb82e95cf36f59a4
moohamedmagdi <moohamedmagdi@gmail.com>
2023-03-16 12:26:34 -0700

    hi there

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Description: print with puts
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
