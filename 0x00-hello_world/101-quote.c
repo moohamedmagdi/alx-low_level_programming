182e57fd285fefcc1de52124872b267600e387c3
moohamedmagdi <moohamedmagdi@gmail.com>
2023-03-16 15:07:57 -0700

    hi there

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - entry
+ *
+ * Description: print without printf
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(1, quo, 59);
+	return (1);
+}
