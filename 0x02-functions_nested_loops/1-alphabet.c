1b0f63427ed6e2dbbd2936e6ffd93354e3908b94
moohamedmagdi <moohamedmagdi@gmail.com>
2023-03-21 14:16:36 -0700

    hi there

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
+ * print_alphabet - a function prints the alphabet, in lowercase
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_times_table - Prints the times table of the input,
+ * print_times_table - Prints the times table of input,
  *                     starting with 0.
  * @n: The value of the times table to be printed.
  */
