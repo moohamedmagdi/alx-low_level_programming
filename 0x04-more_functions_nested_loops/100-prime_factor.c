e22d57859b613b80e6e619355dc2c7ccdf8b244e
moohamedmagdi <moohamedmagdi@gmail.com>
2023-03-23 03:21:46 -0700

    hi there

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,5 +7,16 @@
  */
 int main(void)
 {
+	unsigned long int prime = 612852475143;
+	unsigned long int i = 3;
+
+	while (i <= prime / 2)
+		if (prime % i == 0)
+			prime = prime / i;
+		else
+			i += 2;
+
+	printf("%ld\n", prime);
+
 	return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,16 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
 
-	return (0);
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
