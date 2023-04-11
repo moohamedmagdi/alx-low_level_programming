0327f59e9452db849b40671cfd8411a52158b1cd
moohamedmagdi <moohamedmagdi@gmail.com>
2023-04-11 13:57:58 -0700

    memory allocation

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,37 +11,36 @@
  */
 char *argstostr(int ac, char **av)
 {
-        int x;
-        int y;
-        int z;
-        int ex;
-        char *p = NULL;
+	int x;
+	int y;
+	int z;
+	int ex;
+	char *p = NULL;
 
-        z = 0;
-        ex = 0;
-        if (ac == 0 || av == NULL)
-                return (NULL);
-        for (x = 0; x < ac; x++)
-        {
-                for (y = 0; av[x][y] != '\0'; y++)
-                {
-                        ex++;
-                }
-        }
-
-        p = (char *)malloc(ex + ac + 1 * sizeof(char));
-        if (p == NULL)
-                return (NULL);
-        for (x = 0; x < ac; x++)
-        {
-                for (y = 0; av[x][y] != '\0'; y++)
-                {
-                        p[z] = av[x][y];
-                        z++;
-                }
-                p[z] = '\n';
-                z++;
-        }
-        p[z] = '\0';
-        return (p);
+	z = 0;
+	ex = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			ex++;
+		}
+	}
+	p = (char *)malloc(ex + ac + 1 * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			p[z] = av[x][y];
+			z++;
+		}
+		p[z] = '\n';
+		z++;
+	}
+	p[z] = '\0';
+	return (p);
 }
