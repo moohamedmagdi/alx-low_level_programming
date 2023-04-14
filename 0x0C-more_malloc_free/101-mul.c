1b5b5c187004e134a85a1ae0aaf93f82716a5ae2
moohamedmagdi <moohamedmagdi@gmail.com>
2023-04-13 18:02:47 -0700

    memory allocatation

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,13 +11,13 @@
 
 void _puts(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i])
-        {
-                _putchar(str[i]);
-                i++;
-        }
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
 
 /**
@@ -29,23 +29,22 @@ void _puts(char *str)
 
 int _atoi(const char *s)
 {
-        int sign = 1;
-        unsigned long int resp = 0, firstnum, i;
+	int sign = 1;
+	unsigned long int resp = 0, firstnum, i;
 
-        for (firstnum = 0; !(s[firstnum] >= 48 && s[firstnum] <= 57); firstnum++)
-        {
-                if (s[firstnum] == '-')
-                {
-                        sign *= -1;
-                }
-        }
-        for (i = firstnum; s[i] >= 48 && s[i] <= 57; i++)
-        {
-                resp *= 10;
-                resp += (s[i] - 48);
-        }
-
-        return (sign * resp);
+	for (firstnum = 0; !(s[firstnum] >= 48 && s[firstnum] <= 57); firstnum++)
+	{
+		if (s[firstnum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+	for (i = firstnum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+	return (sign * resp);
 }
 
 /**
@@ -57,16 +56,16 @@ int _atoi(const char *s)
 
 void print_int(unsigned long int n)
 {
-        unsigned long int divisor = 1, i, resp;
+	unsigned long int divisor = 1, i, resp;
 
-        for (i = 0; n / divisor > 9; i++, divisor *= 10)
-        ;
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+	;
 
-        for (; divisor >= 1; n %= divisor, divisor /= 10)
-        {
-                resp = n / divisor;
-                _putchar('0' + resp);
-        }
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
 }
 
 /**
@@ -79,14 +78,14 @@ void print_int(unsigned long int n)
 
 int main(int argc, const char *argv[])
 {
-        (void)argc;
+	(void)argc;
 
-        if (argc != 3)
-        {
-                _puts("Error\n");
-                exit(98);
-        }
-        print_int(_atoi(argv[1]) * _atoi(argv[2]));
-        _putchar('\n');
-        return (0);
+	if (argc != 3)
+	{
+		_puts("Error\n");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+	return (0);
 }
