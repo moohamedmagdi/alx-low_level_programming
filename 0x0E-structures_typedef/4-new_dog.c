d7885c1627a9e1e34ae0eb37bc8100aa0ab33aae
moohamedmagdi <moohamedmagdi@gmail.com>
2023-04-17 15:29:16 -0700

    structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,36 +2,40 @@
 #include <stdlib.h>
 
 /**
- * new_dog - entry point
- * @name: string from main, name of pet
- * @age: number from main, age of pet
- * @owner: string from main, owner of pet
+ * new_dog - create type dog
+ * @name: name
+ * @age: age
+ * @owner: owner
  *
- * Return: p
+ * Return: 
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	int nlen, olen, x;
+	dog_t *doggy;
+
+	nlen = olen = 0;
+	while (name[nlen++])
+	;
+	while (owner[olen++])
+	;
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
 		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
+
+	doggy->name = malloc(nlen * sizeof(doggy->name));
+	if (doggy == NULL)
 		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
+	for (x = 0; x < nlen; x++)
+		doggy->name[x] = name[x];
+	
+	doggy->age = age;
+
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
 		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+	for (x = 0; x < olen; x++)
+		doggy->owner[x] = owner[x];
+	return (doggy);
 }
