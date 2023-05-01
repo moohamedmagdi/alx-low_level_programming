5b17a2e66b0a961849432d87a7721f6100ec61ff
moohamedmagdi <moohamedmagdi@gmail.com>
2023-05-01 04:19:51 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,6 +8,18 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *node = NULL, *next = NULL;
+
+	if (!head || !*head)
+		return (NULL);
+	node = *head;
+	*head = NULL;
+	while (node)
+	{
+		next = node->next;
+		node->next = *head;
+		*head = node;
+		node = next;
+	}
+	return (*head);
 }
