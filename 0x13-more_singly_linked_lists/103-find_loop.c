7dafc22393c822b326d35a89b852805b57f11388
moohamedmagdi <moohamedmagdi@gmail.com>
2023-05-01 05:44:16 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,16 +10,16 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *ptr, *end;
+	listint_t *pt, *end;
 
 	if (head == NULL)
 		return (NULL);
 	for (end = head->next; end != NULL; end = end->next)
 	{
-		if (end == head->next)
+		if (end == end->next)
 			return (end);
-		for (ptr = head; ptr != end; ptr = prt->next)
-			if (ptr == end->next)
+		for (pt = head; pt != end; pt = prt->next)
+			if (pt == end->next)
 				return (end->next);
 	}
 	return (NULL);
