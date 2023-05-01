9b58e246fb0dfc7f6255ea6f9f26d46fe46eb567
moohamedmagdi <moohamedmagdi@gmail.com>
2023-05-01 04:46:45 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
-  * reverse_listint - define function
-  * @head: describe argument
-  * Return: what does it return?
+  * reverse_listint - reverse
+  * @head: head
+  * Return: poi of head
   */
 
 listint_t *reverse_listint(listint_t **head)
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,12 +2,24 @@
 
 /**
   * find_listint_loop - define function
-  * @head:
-  * Return: what does it return?
+  * @head: linked list
+  *
+  * Return: NULL FOR NO LOOP
   */
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	head++;
-	return (0);
+	listint_t *pt, *end;
+
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end = head->next)
+			return (end);
+		for (pt = head; pt != end; pt = pt->next)
+			if (pt == end->next)
+				return (end->next)
+	}
+	return (NULL);
 }
