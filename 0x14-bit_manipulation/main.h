93990391a93d557064fc80986f04cce663994493
moohamedmagdi <moohamedmagdi@gmail.com>
2023-05-04 05:26:43 -0700

    bit manupulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAINN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
