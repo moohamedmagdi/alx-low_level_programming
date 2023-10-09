2f9694efd84ec043bd75b7f4a46c5f56099deda0
moohamedmagdi <moohamedmagdi@gmail.com>
2023-10-09 14:21:32 -0700

    .h modify

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,10 @@ int binary_search(int *array, size_t size, int value);
 size_t midpoint(size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int help_binary(int *array, int value, size_t lo, size_t hi);
+void array_print(int *array, size_t lo, size_t hi);
+int advanced_binary(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 
 #endif /*SEARCH_ALGOS_H*/
